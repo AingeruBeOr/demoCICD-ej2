50934da7dd72c1d38079e5718fe697bd3cc32fd4
Aingeru <81016657+AingeruBeOr@users.noreply.github.com>
2023-11-14 16:15:50 +0100

    Update main.py: Module docstring and UPPER_CASE to variables

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+"""Module docstring."""
+
 def sumar(a,b):
     "Función que suma los números a y b recibidos como parametro"
     return a + b
 
-numero1 = 15
-numero2 = 18
-print(sumar(15, 18))
+NUMERO_1 = 15
+NUMERO_2 = 18
+print(sumar(NUMERO_1, NUMERO_2))
