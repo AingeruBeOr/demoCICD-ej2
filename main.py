e574f6f88c25effbad877dff09eb8a7dc3ea6504
Aingeru <81016657+AingeruBeOr@users.noreply.github.com>
2023-11-14 16:12:41 +0100

    Update main.py: Ident 4 (2 before), 'sum' name to 'sumar' and dockstring added to 'sumar' function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def sum(a,b):
-  return a + b
+def sumar(a,b):
+    "Función que suma los números a y b recibidos como parametro"
+    return a + b
 
 numero1 = 15
 numero2 = 18
-print(sum(15, 18))
+print(sumar(15, 18))
